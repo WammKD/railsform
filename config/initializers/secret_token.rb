693906b6697be433e17a0b44a8f266124380f248
Wamm K. D <jaft@brandeis.edu>
2015-03-10 17:17:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsform::Application.config.secret_key_base = 'ff768cc8cb87784ae4d721c910e90059ec2622070784730169aa1f1ef758bb14b5ec66207a807df4a1b235073cfaec604e02d2d98aec261046b5e3640f310f82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railsform::Application.config.session_store :cookie_store, key: '_railsform_session'
